9d5a71ce65917746b82070beb643da81492e0454
Sameer Bhati <bhati.sameer987@gmail.com>
2025-06-15 14:00:19 +0530

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Sameer Bhati on 15/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EmojiDictionaryTests/EmojiDictionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiDictionaryTests.swift
+//  EmojiDictionaryTests
+//
+//  Created by Sameer Bhati on 15/06/25.
+//
+
+import Testing
+@testable import EmojiDictionary
+
+struct EmojiDictionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
