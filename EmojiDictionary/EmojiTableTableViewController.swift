f30c287a68c49de2aa0ccb3d905af9e5ac8687c3
Sameer Bhati <bhati.sameer987@gmail.com>
2025-06-16 16:55:23 +0530

    second

--- EmojiDictionary/EmojiTableTableViewController.swift
+++ EmojiDictionary/EmojiTableTableViewController.swift
@@ -14,11 +14,11 @@ class EmojiTableTableViewController: UITableViewController {
         Emoji(symbol: "😕", name: "Confused Face", description: "A onfused, puzzled face.", usage: "displeasure"),
         Emoji(symbol: "😍", name: "Heart Eyes", description: "A smileyface with hearts for eyes.", usage: "Attractive"),
         Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.",        usage: "something slow"),
-        Emoji(symbol: "🐘", name: "Elephant", description: "A gray        elephant.", usage: "good memory"),
-        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of        spaghetti.", usage: "spaghetti"),
+        Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
         Emoji(symbol: "🎲", name: "Die", description: "A single die.",        usage: "taking a risk, chance; game"),
         Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.",        usage: "camping"),
-        Emoji(symbol: "📚", name: "Stack of Books", description: "Three        colored books stacked on each other.", usage: "learning"),
+        Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "learning"),
         Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart", usage: "extreme sadness")
     ]
 
@@ -44,25 +44,47 @@ class EmojiTableTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return emojis.count
     }
+    
+    
+    @IBSegueAction func addEditEmoji(_ coder: NSCoder, sender: Any?) -> AddEditEmojiTableViewController? {
+        if let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell)
+        {
+            let emojiToEdit = emojis[indexPath.row]
+            return AddEditEmojiTableViewController(emoji: emojiToEdit, coder: coder)
+        }else{
+            return AddEditEmojiTableViewController(emoji: nil, coder: coder)
+        }
+    }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+//        let emoji = emojis[indexPath.row]
+//        // Configure the cell...
+//
+//        var content = cell.defaultContentConfiguration()
+//        content.text = "\(emoji.symbol) - \(emoji.name)"
+//        content.secondaryText = emoji.description
+//        cell.contentConfiguration = content
+//        cell.showsReorderControl=true
+//        return cell
+//    }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath) as! EmojiTableViewCell
         let emoji = emojis[indexPath.row]
         // Configure the cell...
 
-        var content = cell.defaultContentConfiguration()
-        content.text = "\(emoji.symbol) - \(emoji.name)"
-        content.secondaryText = emoji.description
-        cell.contentConfiguration = content
+        cell.update(with: emoji)
         cell.showsReorderControl=true
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let emoji=emojis[indexPath.row]
-        print("Selected \(emoji.symbol), \(indexPath)")
-    }
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let emoji=emojis[indexPath.row]
+//        print("Selected \(emoji.symbol), \(indexPath)")
+//    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -77,21 +99,18 @@ class EmojiTableTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            emojis.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
     
-    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .none
-    }
 
     
     // Override to support rearranging the table view.
@@ -99,6 +118,22 @@ class EmojiTableTableViewController: UITableViewController {
         let movedEmoji=emojis.remove(at: fromIndexPath.row)
         emojis.insert(movedEmoji, at: to.row)
 
+    }
+    
+    
+    @IBAction func unwindToEmojiTableView(segue: UIStoryboardSegue){
+        guard segue.identifier == "saveUnwind",
+              let sourceViewController=segue.source as? AddEditEmojiTableViewController,
+              let emoji=sourceViewController.emoji else {return}
+        
+        if let selectedIndexPath=tableView.indexPathForSelectedRow{
+            emojis[selectedIndexPath.row]=emoji
+            tableView.reloadRows(at: [selectedIndexPath], with: .none)
+        }else{
+            let newIndexPath=IndexPath(row: emojis.count, section: 0)
+            emojis.append(emoji)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
     }
     
 
