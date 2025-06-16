d99639eb6e7a82bee0be786105d2f345b7909178
Sameer Bhati <bhati.sameer987@gmail.com>
2025-06-16 10:55:33 +0530

    first

---
+++ Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Sameer Bhati on 15/06/25.
+//
+
+import Foundation
+
+struct Emoji{
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
---
+++ EmojiDictionary/EmojiTableTableViewController.swift
@@ -0,0 +1,123 @@
+//
+//  EmojiTableTableViewController.swift
+//  EmojiDictionary
+//
+//  Created by Sameer Bhati on 15/06/25.
+//
+
+import UIKit
+
+class EmojiTableTableViewController: UITableViewController {
+    
+    var emojis: [Emoji] = [
+        Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
+        Emoji(symbol: "😕", name: "Confused Face", description: "A onfused, puzzled face.", usage: "displeasure"),
+        Emoji(symbol: "😍", name: "Heart Eyes", description: "A smileyface with hearts for eyes.", usage: "Attractive"),
+        Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.",        usage: "something slow"),
+        Emoji(symbol: "🐘", name: "Elephant", description: "A gray        elephant.", usage: "good memory"),
+        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of        spaghetti.", usage: "spaghetti"),
+        Emoji(symbol: "🎲", name: "Die", description: "A single die.",        usage: "taking a risk, chance; game"),
+        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.",        usage: "camping"),
+        Emoji(symbol: "📚", name: "Stack of Books", description: "Three        colored books stacked on each other.", usage: "learning"),
+        Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart", usage: "extreme sadness")
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return emojis.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        let emoji = emojis[indexPath.row]
+        // Configure the cell...
+
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(emoji.symbol) - \(emoji.name)"
+        content.secondaryText = emoji.description
+        cell.contentConfiguration = content
+        cell.showsReorderControl=true
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji=emojis[indexPath.row]
+        print("Selected \(emoji.symbol), \(indexPath)")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let movedEmoji=emojis.remove(at: fromIndexPath.row)
+        emojis.insert(movedEmoji, at: to.row)
+
+    }
+    
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
